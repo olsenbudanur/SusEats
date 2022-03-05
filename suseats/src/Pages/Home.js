45b480ff59e2da70340e0b701a15b32860e4ee42
drew-beamer <drbeamer@davidson.edu>
2022-03-05 02:04:34 -0500

    home page proto added

---
+++ suseats/src/Pages/Home.js
@@ -0,0 +1,33 @@
+import { Box, flexbox } from "@mui/system";
+import { AppBar, Button, Grid, Typography } from "@mui/material";
+import Navbar from "../ComponentTemplates/Navbar";
+
+export default function Home() {
+  return (
+    <Box
+      sx={{
+        height: "80vh",
+        background: "linear-gradient(to bottom right, #57CC99, #38A3A5)",
+      }}
+    >
+      <Navbar background="transparent" />
+      <Grid container spacing={2} pt={{xs: 10, sm: 15}} px={2} justifyContent="flex-start">
+        <Grid item xs={12}>
+            <Typography variant={"h1"} sx={{ color: "white" }}>
+              EAT MORE SUS.
+            </Typography>
+        </Grid>
+        <Grid item xs={12}>
+
+          <Button
+            pt={2}
+            variant="outlined"
+            sx={{ color: "white", borderColor: "white" }}
+          >
+            explore options.
+          </Button>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
---
+++ suseats/src/theme.js
@@ -0,0 +1,25 @@
+import { createTheme } from "@mui/material/styles";
+
+
+// palette: https://coolors.co/palette/22577a-38a3a5-57cc99-80ed99-c7f9cc
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#57CC99"
+        },
+        secondary: {
+            main: "#38A3A5"
+        }
+    },
+    typography: {
+        fontFamily: "Fredoka",
+        h1: {
+            fontWeight: 500
+        },
+        button: {
+            fontSize: 18,
+            fontWeight: 300
+        }
+    }
+})
