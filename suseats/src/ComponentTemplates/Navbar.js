1903abfaa44a15a5d6f14e1c90d45b051e1104f4
ebtesam25 <ehaque4@gmu.edu>
2022-03-05 13:24:42 -0500

    navbar logo fixed

--- suseats/src/ComponentTemplates/Navbar.js
+++ suseats/src/ComponentTemplates/Navbar.js
@@ -44,7 +44,7 @@ const Navbar = (props) => {
             component="div"
             sx={{ mr: 2, height: 25, display: { xs: 'none', md: 'flex' } }}
           >
-                        <img src={suseatslogo} />
+                        <img src={suseatslogo} style={{width:200, height:'auto'}} />
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
